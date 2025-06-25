ed2159ebb10cbab124b154a64bff333b8037216d
Jakob Lingel <jlingel68@gmail.com>
2025-06-25 22:19:11 +0200

    Try skipping generation of graph at build time

--- src/app/graph/page.tsx
+++ src/app/graph/page.tsx
@@ -5,6 +5,10 @@ import { Suspense } from 'react';
 import Loading from 'app/loading';
 
 export default async function GraphPage() {
+  if (process.env.SKIP_BUILD_STATIC_GENERATION === 'true') {
+    console.warn('Skipping static generation for GraphPage');
+    return <div>Skipped at build time</div>;
+  }
   return (
     <div className="grid gap-4 p-8 pb-20 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <button className="rounded-full bg-slate-200 px-2 w-fit text-slate-800"><code><Link href="/">back</Link></code></button>
