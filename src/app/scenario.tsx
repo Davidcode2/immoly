7847c944ed78fbda1ba77493fa5006d76da2eb51
Jakob Lingel <jlingel68@gmail.com>
2025-07-22 23:25:28 +0200

    Add stupid gemini joke

--- src/app/scenario.tsx
+++ src/app/scenario.tsx
@@ -45,12 +45,22 @@ export default function Scenario({
   const paidInYear = new Date().getFullYear() + paidAfter;
 
   return (
-    <div className="mx-auto md:mx-0 my-16 md:mb-4 md:mt-0 border border-purple-500 backdrop-blur-2xl rounded-lg max-w-60 bg-blur">
-      <ScenarioTextDisplay
-        sumZinsen={sumZinsen}
-        paidAfter={paidAfter}
-        paidInYear={paidInYear}
-      />
+    <div className="grid grid-cols-2 gap-4">
+      <div className="md:mx-0 my-16 md:mb-4 md:mt-0 border border-purple-500 backdrop-blur-2xl rounded-lg md:max-w-60 bg-blur">
+        <ScenarioTextDisplay
+          sumZinsen={sumZinsen}
+          paidAfter={paidAfter}
+          paidInYear={paidInYear}
+        />
+      </div>
+      <div className="md:hidden my-16 border border-purple-500/70 backdrop-blur-xl rounded-lg bg-blur flex justify-center items-center p-8 flex-col">
+        Why did the scarecrow win an award? 🏆 Because he was outstanding in his
+        field! 🧑‍🌾😂
+        <div>
+          <div className="font-cursive text-xs pt-2 italic">Gemini</div>
+          <div className="font-cursive text-xs"> - thinks this is funny</div>
+        </div>
+      </div>
     </div>
   );
 }
