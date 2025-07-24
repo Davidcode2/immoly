a5b52109e8e343a8a2b3f40e434683c65865e898
Jakob Lingel <jlingel68@gmail.com>
2025-07-24 08:05:07 +0200

    Style: add padding to top and bottom on mobile

--- src/app/graphs.tsx
+++ src/app/graphs.tsx
@@ -5,7 +5,7 @@ export default async function Graphs() {
   const calculationData = await getCalculations();
 
   return (
-    <div className="flex max-w-screen items-stretch gap-16 overflow-auto px-3">
+    <div className="flex max-w-screen items-stretch gap-16 overflow-auto px-3 pb-3">
       {calculationData &&
         calculationData.map((calcResult) => (
           <SingleGraph key={calcResult.id} calculation={calcResult} />
