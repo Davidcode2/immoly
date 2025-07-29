86f764289730edceb8a861627c399b8a351807a0
Jakob Lingel <jlingel68@gmail.com>
2025-07-29 08:35:49 +0200

    Revert "Sondertilgung onChange experiment"
    This reverts commit 8ce2a050d1bead1e4d02e1906d5736d9e8d2448a.

--- src/app/components/sondertilgungInput.tsx
+++ src/app/components/sondertilgungInput.tsx
@@ -3,25 +3,14 @@ import { useEffect, useState } from "react";
 type PropTypes = {
   sondertilgung: string;
   year: number;
-  onChange: (amount: number, year: number) => Promise<void>;
 };
 
-export default function SondertilgungInput({
-  sondertilgung,
-  year,
-  onChange,
-}: PropTypes) {
+export default function SondertilgungInput({ sondertilgung }: PropTypes) {
   const [amount, setAmount] = useState<string | null>(sondertilgung);
 
   useEffect(() => {
     setAmount(sondertilgung);
   }, [sondertilgung]);
-
-  /* eslint-disable  @typescript-eslint/no-explicit-any */
-  const localHandelChange = async (e: any) => {
-    setAmount(String(e.target.value));
-    await onChange(Number(amount), year);
-  };
 
   return (
     <input
@@ -29,7 +18,7 @@ export default function SondertilgungInput({
       type="string"
       name="sonderTilgungAmount"
       value={amount!}
-      onChange={localHandelChange}
+      onChange={(e) => setAmount(String(e.target.value))}
       className={`${amount ? "text-green-400" : "text-gray-800"} w-20 rounded-md p-[3px] text-sm active:text-gray-200`}
     />
   );
