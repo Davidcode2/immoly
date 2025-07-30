b324293c1b6af762573443fc38fbd472460fe337
Jakob Lingel <jlingel68@gmail.com>
2025-07-30 22:32:46 +0200

    Style: tilgungs modal

--- src/app/components/tilgungsWechselModal.tsx
+++ src/app/components/tilgungsWechselModal.tsx
@@ -7,22 +7,24 @@ export default function TilgungsWechselModal({
   year,
 }: PropTypes) {
   return (
-    <div className="tilgungsWechselModal flex h-[300px] w-[400px] flex-col gap-4 rounded-xl bg-purple-500 p-10 shadow">
-      <label className="text-xl" htmlFor="newTilgung">
-        Neue Rate ab {year}
-      </label>
-
-      <form
-        onSubmit={(e) => handleSubmit(e, year)}
-        className="justify-fit flex sm:gap-4"
-      >
-        <input
-          className="tilgungsWechselModal w-36 rounded-lg border-b border-stone-700 bg-transparent pb-1 text-xl transition-colors duration-200 focus:border-slate-200 focus:outline-none md:text-base"
-          id="newTilgung"
-          name="newTilgung"
-          type="text"
-        />
-      </form>
+    <div className="tilgungsWechselModal flex h-[300px] w-[400px] flex-col items-center gap-4 rounded-xl bg-gradient-to-tl from-purple-900/90 to-purple-950/90 p-10 shadow">
+      <div className="my-auto flex flex-col gap-4">
+        <label className="text-xl px-10 text-center" htmlFor="newTilgung">
+          Wählen Sie eine neue Rate nach {year <= 1 ? "einem Jahr" : year + " Jahren"}
+        </label>
+        <form
+          onSubmit={(e) => handleSubmit(e, year)}
+          className="flex justify-center"
+        >
+          <input
+            className="tilgungsWechselModal w-36 border-b border-stone-700 bg-transparent pb-1 text-xl transition-colors duration-200 focus:border-slate-200 focus:outline-none md:text-2xl"
+            id="newTilgung"
+            name="newTilgung"
+            type="text"
+          />
+          <div className="relative -left-6 top-2 text-lg text-stone-600">€</div>
+        </form>
+      </div>
     </div>
   );
 }
