a28d4de44adf41a9367a21d853192877e122e803
Jakob Lingel <jlingel68@gmail.com>
2025-08-03 18:21:03 +0200

    Warning when eigenkapital lower than nebenkosten

--- src/app/components/kreditSummeTextComponent.tsx
+++ src/app/components/kreditSummeTextComponent.tsx
@@ -1,4 +1,5 @@
 import NebenkostenCalculator from "app/services/nebenkostenCalculationService";
+import Image from "next/image";
 
 type PropTypes = {
   principal: number;
@@ -12,18 +13,29 @@ export default function KreditSummeTextComponent({
   const kreditSumme = principal + nebenkosten - downPayment;
 
   return (
-    <div className="grid grid-cols-2 items-baseline gap-x-2 rounded-lg px-6 pt-6 shadow backdrop-blur-2xl">
+    <div className="grid grid-cols-2 items-baseline gap-x-2 rounded-lg px-6 pt-6 shadow backdrop-blur-2xl z-20">
       <span className="text-end text-lg">{principal.toLocaleString("de")}</span>
       <span className="">Kaufpreis</span>
       <span className="text-end">+ {nebenkosten.toLocaleString("de")}</span>
       <span className="text-xs">Nebenkosten</span>
       <span className="text-end">- {downPayment.toLocaleString("de")}</span>
-      <span className="text-xs">Eigenkapital</span>
-      {nebenkosten > downPayment && (
-        <span className="col-span-2 text-center -mt-2 text-xs text-red-500">
-          Eigenkapital deckt Nebenkosten nicht.<br/>Könnte schwierig werden.
-        </span>
-      )}
+      <span className="group flex items-center gap-x-2 text-xs relative">
+        Eigenkapital
+        {nebenkosten > downPayment && (
+          <>
+            <Image
+              src="/images/icons/icons8-warnung-emoji-48.png"
+              alt="yellow warning icon with exclamation point"
+              width={24}
+              height={24}
+            />
+            <span className="bg-white dark:bg-black left-28 w-60 p-4 rounded-lg shadow-lg absolute opacity-0 text-base text-violet-400 group-hover:opacity-100">
+              Eigenkapital deckt Nebenkosten nicht.<br/>
+              Könnte schwierig werden.
+            </span>
+          </>
+        )}
+      </span>
       <div className="text-end text-xl font-bold text-pink-500">
         {kreditSumme.toLocaleString("de")}
       </div>
