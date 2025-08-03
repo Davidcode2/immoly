c388a2a237895c0b2e34fd0bf0ea5b47fae11520
Jakob Lingel <jlingel68@gmail.com>
2025-08-03 18:02:09 +0200

    Reorder kreditsumme calculation - move eigenkapital below nebenkosten

--- src/app/components/kreditSummeTextComponent.tsx
+++ src/app/components/kreditSummeTextComponent.tsx
@@ -15,10 +15,15 @@ export default function KreditSummeTextComponent({
     <div className="grid grid-cols-2 items-baseline gap-x-2 rounded-lg px-6 pt-6 shadow backdrop-blur-2xl">
       <span className="text-end text-lg">{principal.toLocaleString("de")}</span>
       <span className="">Kaufpreis</span>
-      <span className="text-end">- {downPayment.toLocaleString("de")}</span>
-      <span className="text-xs">Eigenkapital</span>
       <span className="text-end">+ {nebenkosten.toLocaleString("de")}</span>
       <span className="text-xs">Nebenkosten</span>
+      <span className="text-end">- {downPayment.toLocaleString("de")}</span>
+      <span className="text-xs">Eigenkapital</span>
+      {nebenkosten > downPayment && (
+        <span className="col-span-2 text-center -mt-2 text-xs text-red-500">
+          Eigenkapital deckt Nebenkosten nicht.<br/>Könnte schwierig werden.
+        </span>
+      )}
       <div className="text-end text-xl font-bold text-pink-500">
         {kreditSumme.toLocaleString("de")}
       </div>
