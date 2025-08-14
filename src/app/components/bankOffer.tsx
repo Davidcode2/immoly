a0b8c6ef9b7933ec738dd1725cbf0b0abb072957
Jakob Lingel <jlingel68@gmail.com>
2025-08-14 18:57:23 +0200

    Adjust bank offer for mobile

--- src/app/components/bankOffer.tsx
+++ src/app/components/bankOffer.tsx
@@ -4,22 +4,29 @@ type PropTypes = {
   title: string;
   interest: number;
   zinsbindung: number;
-  imageSrc: string
-  sondertilgungen?: number
+  imageSrc: string;
+  sondertilgungen?: number;
 };
-export default function BankOffer({ title, interest, zinsbindung, imageSrc, sondertilgungen }: PropTypes) {
+export default function BankOffer({
+  title,
+  interest,
+  zinsbindung,
+  imageSrc,
+  sondertilgungen,
+}: PropTypes) {
   return (
-    <div className="flex gap-4 rounded-lg shadow">
-      <Image
-        className="rounded-l-lg"
-        src={imageSrc}
-        height="80"
-        width="228"
-        alt="BKB logo"
-      />
-      <div className="p-4 h-60 w-full overflow-scroll">
+    <div className="flex flex-col gap-4 rounded-lg shadow md:flex-row">
+      <div className="relative h-40 md:h-auto md:w-96">
+        <Image
+          className="w-fit rounded-t-lg object-cover object-center md:rounded-l-lg md:rounded-t-none"
+          fill
+          src={imageSrc}
+          alt="BKB logo"
+        />
+      </div>
+      <div className="h-60 w-full overflow-scroll p-4">
         <div className="pb-4 text-2xl">{title}</div>
-        <div className="grid md:grid-cols-[repeat(4,180px)] gap-4">
+        <div className="grid gap-4 md:grid-cols-[repeat(4,180px)]">
           <div className="rounded-lg bg-[var(--secondary)]/50 p-3 shadow">
             <div className="text-xs">Zins</div>
             <div>{interest} %</div>
@@ -28,12 +35,12 @@ export default function BankOffer({ title, interest, zinsbindung, imageSrc, sond
             <div className="text-xs">Zinsbindung</div>
             <div>{zinsbindung} Jahre</div>
           </div>
-          { sondertilgungen && 
-          <div className="rounded-xl bg-[var(--alert)]/50 p-3 shadow">
-            <div className="text-xs">Sondertilgung</div>
-            <div>{sondertilgungen} %</div>
-          </div>
-          }
+          {sondertilgungen && (
+            <div className="rounded-xl bg-[var(--alert)]/50 p-3 shadow">
+              <div className="text-xs">Sondertilgung</div>
+              <div>{sondertilgungen} %</div>
+            </div>
+          )}
         </div>
       </div>
     </div>
