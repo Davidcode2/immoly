b6a3029399bf818de0047a866941804687beb4f3
Jakob Lingel <jlingel68@gmail.com>
2025-08-16 15:36:58 +0200

    Increment buttons on slider

--- src/app/components/slider/slider.tsx
+++ src/app/components/slider/slider.tsx
@@ -1,22 +1,33 @@
 import { useEffect, useState } from "react";
-import './slider.css';
+import "./slider.css";
 
 type SliderProps = {
   value: number;
   min: number;
   max: number;
   step?: number;
-  inputName: string,
+  inputName: string;
   onChange: (name: string, value: number) => void;
 };
 
-const Slider: React.FC<SliderProps> = ({ value, min, max, step = 1, inputName, onChange }) => {
+const Slider: React.FC<SliderProps> = ({
+  value,
+  min,
+  max,
+  step = 1,
+  inputName,
+  onChange,
+}) => {
   const [inputValue, setInputValue] = useState(value);
 
-  const localChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const newValue = Number(e.target.value);
+    localChange(newValue, e.target.name);
+  };
+
+  const localChange = (newValue: number, inputName: string) => {
     setInputValue(newValue);
-    onChange(e.target.name, Number(e.target.value));
+    onChange(inputName, newValue);
   }
 
   useEffect(() => {
@@ -25,22 +36,35 @@ const Slider: React.FC<SliderProps> = ({ value, min, max, step = 1, inputName, o
     }, 30);
     return () => {
       clearTimeout(debouncedChange);
-    }
+    };
   }, [value]);
 
+  const stepChange = (upOrDown: number) => {
+    const up = upOrDown === 1;
+    if (up) {
+      localChange(inputValue + step, inputName);
+    }
+    else {
+      localChange(inputValue - step, inputName);
+    }
+  }
+
   return (
-    <input
-      type="range"
-      value={inputValue}
-      min={min}
-      max={max}
-      step={step}
-      onChange={localChange}
-      name={inputName}
-      className="mt-1"
-    />
+    <div className="flex relative">
+      <button className="absolute -left-8 px-2 top-[1px] opacity-50 lg:hidden hover:opacity-100 hover:border border-[var(--accent)] rounded-lg" onClick={() => stepChange(-1)} type="button">-</button>
+      <input
+        type="range"
+        value={inputValue}
+        min={min}
+        max={max}
+        step={step}
+        onChange={handleInputChange}
+        name={inputName}
+        className="mt-1"
+      />
+      <button className="absolute -right-8 top-[1px] px-2 opacity-50 lg:hidden hover:opacity-100 hover:border border-[var(--accent)] rounded-lg" onClick={() => stepChange(1)} type="button">+</button>
+    </div>
   );
 };
 
 export default Slider;
-
