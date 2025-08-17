94f5d1f87436cf30372c3cbc69b261025b49efaf
Jakob Lingel <jlingel68@gmail.com>
2025-08-17 14:25:20 +0200

    Germany Map hover bundeslaender

--- src/app/components/nebenkosten/nebenkostenModal.tsx
+++ src/app/components/nebenkosten/nebenkostenModal.tsx
@@ -1,4 +1,7 @@
+import GermanyMap from "../germanyMap";
 import PieChartNebenkosten from "./pieChartNebenkosten";
+//import GermanyMap from "/public/images/germany-outline-borders.svg"
+import Image from "next/image";
 
 type PropTypes = {
   /* eslint-disable  @typescript-eslint/no-explicit-any */
@@ -16,53 +19,56 @@ export default function NebenkostenModal({
   handleMouseLeave,
 }: PropTypes) {
   return (
-    <div className="z-40 mx-auto max-w-md rounded-xl border border-slate-500/20 bg-radial-[at_50%_75%] from-[var(--primary)]/50 to-[var(--primary)]/40 shadow-2xl">
-      <div className="p-6">
-        <PieChartNebenkosten
-          data={nebenkosten}
-          activeIndex={activeIndex}
-          handleMouseEnter={handleMouseEnter}
-          handleMouseLeave={handleMouseLeave}
-        />
-      </div>
+    <div className="z-40 mx-auto max-w-xl rounded-xl border border-slate-500/20 bg-radial-[at_50%_75%] from-[var(--background)]/50 to-[var(--primary)]/20 shadow-2xl">
+      <div className="grid grid-cols-2">
+        <GermanyMap/>
+        <div>
+          <div className="p-6">
+            <PieChartNebenkosten
+              data={nebenkosten}
+              activeIndex={activeIndex}
+              handleMouseEnter={handleMouseEnter}
+              handleMouseLeave={handleMouseLeave}
+            />
+          </div>
 
-      <div className="space-y-4 px-6 pb-6">
-        {nebenkosten.map(
-          (entry: { name: string; value: number }, index: number) => (
-            <div
-              key={entry.name}
-              onMouseEnter={() => handleMouseEnter(index)}
-              onMouseLeave={handleMouseLeave}
-              className={`group flex cursor-pointer items-center justify-between rounded-lg p-3 transition-all hover:bg-white/5 ${
-                activeIndex === index
-                  ? "text-[var(--accent)] opacity-100"
-                  : "opacity-90"
-              }`}
-            >
-              <div className="flex items-center gap-3">
-                <span
-                  className="h-4 w-4 rounded-sm transition-transform group-hover:scale-110"
-                  style={{
-                    backgroundColor: `var(--${index === 0 ? "dark-accent" : index === 1 ? "neutral-accent" : index === 2 ? "accent" : "light-accent"})`,
-                  }}
-                />
-                <div className="text-lg font-medium md:text-base">
-                  {entry.name}
+          <div className="space-y-4 px-6 pb-6">
+            {nebenkosten.map(
+              (entry: { name: string; value: number }, index: number) => (
+                <div
+                  key={entry.name}
+                  onMouseEnter={() => handleMouseEnter(index)}
+                  onMouseLeave={handleMouseLeave}
+                  className={`group flex cursor-pointer items-center justify-between rounded-lg p-3 transition-all hover:bg-white/5 ${
+                    activeIndex === index
+                      ? "text-[var(--accent)] opacity-100"
+                      : "opacity-90"
+                  }`}
+                >
+                  <div className="flex items-center gap-3">
+                    <span
+                      className="h-4 w-4 rounded-sm transition-transform group-hover:scale-110"
+                      style={{
+                        backgroundColor: `var(--${index === 0 ? "dark-accent" : index === 1 ? "neutral-accent" : index === 2 ? "accent" : "light-accent"})`,
+                      }}
+                    />
+                    <div className="text-lg md:text-base">{entry.name}</div>
+                  </div>
+                  <div className="text-xl md:text-lg">
+                    €{entry.value.toLocaleString("de")}
+                  </div>
                 </div>
-              </div>
-              <div className="text-xl font-semibold md:text-lg">
-                €{entry.value.toLocaleString("de")}
+              ),
+            )}
+
+            <div className="mt-6 border-t border-slate-500/20 pt-4">
+              <div className="flex items-center justify-between gap-x-8">
+                <h2 className="text-xl">Summe Nebenkosten</h2>
+                <p className="text-2xl font-bold text-[var(--accent)]">
+                  €{sumNebenkosten.toLocaleString("de-DE")}
+                </p>
               </div>
             </div>
-          ),
-        )}
-
-        <div className="mt-6 border-t border-slate-500/20 pt-4">
-          <div className="flex items-center justify-between">
-            <h2 className="text-xl font-bold">Summe Nebenkosten</h2>
-            <p className="text-2xl font-bold text-[var(--accent)]">
-              €{sumNebenkosten.toLocaleString("de-DE")}
-            </p>
           </div>
         </div>
       </div>
