3c35d9b4707d7482c01e95b7b9b0346dd76edec4
Jakob Lingel <jlingel68@gmail.com>
2025-08-20 19:09:29 +0200

    Kreditsumme split style

--- src/app/components/kreditSummeTextComponent.tsx
+++ src/app/components/kreditSummeTextComponent.tsx
@@ -12,35 +12,35 @@ export default function KreditSummeTextComponent({
   downPayment,
 }: PropTypes) {
   let nebenkosten = new NebenkostenCalculator(principal).calcSumme();
-  nebenkosten = useNebenkostenStore((state) => state.value)
+  nebenkosten = useNebenkostenStore((state) => state.value);
   const kreditSummeRaw = principal + nebenkosten - downPayment;
   const kreditSumme = kreditSummeRaw < 0 ? 0 : kreditSummeRaw;
 
   return (
-    <div className="z-20 rounded-lg p-5 shadow backdrop-blur-2xl md:p-8">
-      <div className="grid md:grid-cols-[1fr_1fr] items-baseline md:gap-2">
-        <span className="hidden text-end text-lg md:block">
-          {principal.toLocaleString("de")}
-        </span>
-        <span className="hidden md:block">Kaufpreis</span>
-        <span className="flex justify-end gap-x-2 md:max-h-6 text-center grow-0">
-          <div className="h-10 w-10 self-center -rotate-22">
+    <div className="z-20 rounded-lg shadow backdrop-blur-2xl">
+      <div className="p-5 md:p-8">
+        <div className="flex justify-between">
+          <p className="pb-2 text-xs">Kreditsumme</p>
+          <div className="absolute right-4 top-2 h-10 w-10 rotate-180 self-center">
             <PieChartGesamtBetrag
               kreditSumme={kreditSumme}
               downPayment={downPayment}
               kaufSumme={principal}
             />
           </div>
-          <div>
-            <div className="self-center">+&nbsp;{nebenkosten.toLocaleString("de")}</div>
-            <span className="text-end text-xs md:hidden self-center md:text-start">Nebenkosten</span>
-          </div>
+        </div>
+        <div
+          className={`${kreditSumme >= 100000 ? "text-2xl" : "text-3xl"} text-end font-bold text-[var(--accent)] md:text-xl`}
+        >
+          € {kreditSumme.toLocaleString("de")}
+        </div>
+      </div>
+      <div className="grid grid-cols-[1fr_1fr] items-baseline gap-x-2 rounded-b-lg bg-[var(--light-accent)]/20 p-5 pt-2 md:p-8">
+        <span className="text-(length:--text-2xs) text-[var(--dark-accent)]/90">Kaufpreis</span>
+        <span className="text-end text-xs">
+          {principal.toLocaleString("de")}
         </span>
-        <span className="text-end text-xs hidden md:block self-center md:text-start">Nebenkosten</span>
-        <span className="text-end">
-          -&nbsp;{downPayment.toLocaleString("de")}
-        </span>
-        <span className="group relative flex items-center justify-end gap-x-2 text-end text-xs md:col-start-2 md:justify-start md:text-start">
+        <span className="text-[var(--dark-accent)]/90 group relative flex items-center gap-x-2 text-start text-(length:--text-2xs) md:col-start-2 md:justify-start">
           Eigenkapital
           {nebenkosten > downPayment && (
             <>
@@ -50,7 +50,7 @@ export default function KreditSummeTextComponent({
                 width={24}
                 height={24}
               />
-              <span className="invisible absolute left-28 w-60 rounded-lg bg-white p-4 text-base text-[var(--accent)] shadow-lg group-hover:visible dark:bg-black">
+              <span className="invisible absolute left-28 w-60 rounded-lg bg-white p-4 text-xs text-[var(--accent)] shadow-lg group-hover:visible dark:bg-black">
                 Eigenkapital deckt Nebenkosten nicht.
                 <br />
                 Könnte schwierig werden.
@@ -58,14 +58,15 @@ export default function KreditSummeTextComponent({
             </>
           )}
         </span>
-        <div
-          className={`${kreditSumme >= 100000 ? "text-2xl" : "text-3xl"} text-end font-bold text-[var(--accent)] md:text-xl`}
-        >
-          {kreditSumme.toLocaleString("de")}
-        </div>
-        <span className="text-end text-xs md:col-start-2 md:text-start md:text-base">
-          Kreditsumme
+        <span className="text-end text-xs">
+          -&nbsp;{downPayment.toLocaleString("de")}
         </span>
+        <span className="text-[var(--dark-accent)]/90 self-center text-end text-(length:--text-2xs) md:text-start">
+          Nebenkosten
+        </span>
+        <div className="text-end text-xs">
+          +&nbsp;{nebenkosten.toLocaleString("de")}
+        </div>
       </div>
     </div>
   );
