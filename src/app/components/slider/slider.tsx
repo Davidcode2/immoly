f32062ecec391f135106c7bf2d041cb3e8a773ff
Jakob Lingel <jlingel68@gmail.com>
2025-09-02 21:58:23 +0200

    Try fixing position of step buttons on sliders

--- src/app/components/slider/slider.tsx
+++ src/app/components/slider/slider.tsx
@@ -28,7 +28,7 @@ const Slider: React.FC<SliderProps> = ({
   const localChange = (newValue: number, inputName: string) => {
     setInputValue(newValue);
     onChange(inputName, newValue);
-  }
+  };
 
   useEffect(() => {
     const debouncedChange = setTimeout(() => {
@@ -43,15 +43,20 @@ const Slider: React.FC<SliderProps> = ({
     const up = upOrDown === 1;
     if (up) {
       localChange(inputValue + step, inputName);
-    }
-    else {
+    } else {
       localChange(inputValue - step, inputName);
     }
-  }
+  };
 
   return (
-    <div className="flex relative">
-      <button className="absolute -left-8 px-2 top-[1px] opacity-50 lg:hidden hover:opacity-100 hover:border border-[var(--accent)] rounded-lg" onClick={() => stepChange(-1)} type="button">-</button>
+    <div className="sm:mx-0 -mx-5 flex">
+      <button
+        className="rounded-lg border-[var(--accent)] px-2 opacity-50 hover:border hover:opacity-100 lg:hidden"
+        onClick={() => stepChange(-1)}
+        type="button"
+      >
+        -
+      </button>
       <input
         type="range"
         value={inputValue}
@@ -62,7 +67,13 @@ const Slider: React.FC<SliderProps> = ({
         name={inputName}
         className="mt-1"
       />
-      <button className="absolute -right-8 top-[1px] px-2 opacity-50 lg:hidden hover:opacity-100 hover:border border-[var(--accent)] rounded-lg" onClick={() => stepChange(1)} type="button">+</button>
+      <button
+        className="rounded-lg border-[var(--accent)] px-2 opacity-50 hover:border hover:opacity-100 lg:hidden"
+        onClick={() => stepChange(1)}
+        type="button"
+      >
+        +
+      </button>
     </div>
   );
 };
