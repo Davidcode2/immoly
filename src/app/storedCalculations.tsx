8551c354488779095089c7f146a1f4c4da297a66
Jakob Lingel <jlingel68@gmail.com>
2025-09-03 18:38:38 +0200

    Show text if no stored calculations

--- src/app/storedCalculations.tsx
+++ src/app/storedCalculations.tsx
@@ -13,12 +13,21 @@ export default function StoredCalculations() {
   }, []);
 
   return (
-    <div className="h-[800px] bg-[var(--light-accent)]/50 grid grid-rows-[1fr_auto_1fr]">
+    <div className="grid h-[800px] grid-rows-[1fr_auto_1fr] bg-[var(--light-accent)]/50">
       <div className="row-start-2 flex max-w-screen items-stretch gap-16 overflow-auto px-3 pb-3">
-        {calculations &&
+        {false ? (
           calculations.map((calcResult) => (
             <StoredScenario key={calcResult.id} calculation={calcResult} />
-          ))}
+          ))
+        ) : (
+          <div className="mx-auto text-center text-[var(--foreground)]/70">
+            <p className="mb-4 text-2xl">Keine gespeicherten Szenarien</p>
+            <p className="text-lg">
+              Berechne eine Immobilie und speichere dein Szenario, um es hier zu
+              sehen.
+            </p>
+          </div>
+        )}
       </div>
     </div>
   );
