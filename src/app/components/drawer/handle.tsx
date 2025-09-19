622f93b1cfe00178f49d193bd7a34eef0531ecfc
Jakob Lingel <jlingel68@gmail.com>
2025-09-19 18:19:25 +0200

    Larger handle on mobile form

--- src/app/components/drawer/handle.tsx
+++ src/app/components/drawer/handle.tsx
@@ -5,7 +5,7 @@ type HandleProps = {
 export function Handle({ className = "" }: HandleProps) {
   return (
     <div className="absolute rounded-t-2xl w-full bg-linear-to-b from-[var(--background)] to-[var(--background)/90 px-4 py-4 cursor-grab active:cursor-grabbing">
-      <div className={`h-1.5 w-16 mx-auto rounded-full bg-gray-300 ${className}`} />
+      <div className={`h-2 w-20 mx-auto rounded-full bg-gray-300 ${className}`} />
     </div>
   );
 }
