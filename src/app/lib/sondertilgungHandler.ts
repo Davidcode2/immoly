68dd43a261f711168d4dc6ef1aea445f72df5502
Jakob Lingel <jlingel68@gmail.com>
2025-09-22 22:15:48 +0200

    Delete unused code

--- src/app/lib/sondertilgungHandler.ts
+++ src/app/lib/sondertilgungHandler.ts
@@ -1,6 +1,4 @@
-import calcTilgung from "./calculateArmotizaztionTable";
 import ArmotizationEntry from "./models/ArmotizationEntry";
-import CashRoiModel from "./models/cashRoiModel";
 import { Sondertilgung } from "./models/sondertilgung";
 
 export const updateSondertilgungInTable = (
@@ -29,77 +27,4 @@ export const updateSondertilgungForYear = (
   const copy = [...sondertilgungen];
   copy[currentSondertilgungIndex].amount = amount;
   return copy;
-};
-
-export const recalcForAllSondertilgungen = async (
-  sondertilgungen: Sondertilgung[] | undefined,
-  tilgungsTabelle: ArmotizationEntry[],
-  calculationValues: CashRoiModel | null,
-) => {
-  if (!sondertilgungen) {
-    return tilgungsTabelle;
-  }
-  sondertilgungen.sort((a, b) => a.year - b.year);
-  if (
-    calculationValues &&
-    calculationValues.interest_rate &&
-    calculationValues.monthly_rate
-  ) {
-    const newTable = [...tilgungsTabelle];
-    tilgungsTabelle.forEach((tableRow: ArmotizationEntry) => {
-      const tableRowNew = newTable.find((x) => x.year === tableRow.year);
-      if (!tableRowNew) {
-        return newTable;
-      }
-      const sondertilgung = sondertilgungen.find(
-        (y) => y.year === tableRow.year,
-      );
-      if (sondertilgung) {
-        recalcRestOfTableWithSondertilgung(
-          sondertilgung,
-          sondertilgungen,
-          tilgungsTabelle,
-          calculationValues,
-          tableRow,
-          newTable,
-          tableRowNew
-        );
-      }
-    });
-    return newTable;
-  } else {
-    return tilgungsTabelle;
-  }
-};
-
-const recalcRestOfTableWithSondertilgung = (
-  sondertilgung: Sondertilgung,
-  sondertilgungen: Sondertilgung[],
-  tilgungsTabelle: ArmotizationEntry[],
-  calculationValues: CashRoiModel,
-  tableRow: ArmotizationEntry,
-  newTable: ArmotizationEntry[],
-  tableRowNew: ArmotizationEntry,
-) => {
-  const newReducedPrincipal =
-    tableRowNew.remainingPrincipal - sondertilgung.amount;
-  if (newReducedPrincipal < 0) {
-    return newTable;
-  }
-  const newTilgung = calcTilgung({
-    principal: newReducedPrincipal,
-    down_payment: 0,
-    interest_rate: calculationValues?.interest_rate,
-    monthly_rate: calculationValues?.monthly_rate,
-    rent: calculationValues?.rent || 0,
-    sondertilgungen: sondertilgungen,
-  });
-  newTilgung.forEach((x: ArmotizationEntry) => {
-    x.year = x.year + tableRow.year;
-  });
-  newTable.splice(
-    tilgungsTabelle.indexOf(tableRow) + 1,
-    newTable.length,
-    ...newTilgung,
-  );
 };
