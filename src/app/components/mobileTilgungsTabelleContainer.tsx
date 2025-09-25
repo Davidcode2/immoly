247d5a7d7906cb7563c98f2e906a1bb92145f6a8
Jakob Lingel <jlingel68@gmail.com>
2025-09-25 19:11:17 +0200

    Steps number dark color in dark mode

--- src/app/components/mobileTilgungsTabelleContainer.tsx
+++ src/app/components/mobileTilgungsTabelleContainer.tsx
@@ -18,11 +18,11 @@ export default function MobileTilgungsTabelleContainer({
     <div className="mx-4 my-14 sm:mx-0 md:hidden">
       <div className="grid grid-cols-2 gap-4 my-4">
         <div className="shadow p-4 grid gap-2 sm:grid-cols-[50px_1fr] rounded-lg">
-          <div className="p-2 h-10 w-10 text-center rounded-full bg-[var(--primary)]">1</div>
+          <div className="p-2 h-10 w-10 text-center rounded-full bg-[var(--primary)] dark:text-[var(--background)]">1</div>
           <div>Tippe auf einen Eintrag um einen Tilgungswechsel vorzunehmen</div>
         </div>
         <div className="shadow p-4 grid gap-2 sm:grid-cols-[50px_1fr] rounded-lg">
-          <div className="p-2 h-10 w-10 text-center rounded-full bg-[var(--primary)]">2</div>
+          <div className="p-2 h-10 w-10 text-center rounded-full bg-[var(--primary)] dark:text-[var(--background)]">2</div>
           <div>Erstelle eine Sondertilgung mit dem Pluszeichen</div>
         </div>
       </div>
