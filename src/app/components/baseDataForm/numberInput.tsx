2a4b683fb9f1deec6a0a1d4c9aa5f45481a0cc4f
Jakob Lingel <jlingel68@gmail.com>
2025-09-25 17:18:08 +0200

    Change validation color

--- src/app/components/baseDataForm/numberInput.tsx
+++ src/app/components/baseDataForm/numberInput.tsx
@@ -118,7 +118,7 @@ export default function NumberInput({
       name={inputName}
       onChange={localHandleChange}
       placeholder="Zahl eingeben"
-      className={` ${!formattedValidationPattern().test(displayValue) ? "text-green-900" : ""} w-36 border-b border-[var(--secondary)] bg-transparent pb-1 text-xl transition-colors duration-200 invalid:text-green-900 focus:border-[var(--accent)] focus:outline-none lg:text-base`}
+      className={` ${!formattedValidationPattern().test(displayValue) ? "text-[var(--ultralight-accent)]" : ""} w-36 border-b border-[var(--secondary)] bg-transparent pb-1 text-xl transition-colors duration-200 invalid:text-green-900 focus:border-[var(--accent)] focus:outline-none lg:text-base`}
     />
   );
 }
