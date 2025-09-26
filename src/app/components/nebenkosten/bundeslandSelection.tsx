55dbff5132f4f87fcef1d258ca5c1e4bba9321bb
Jakob Lingel <jlingel68@gmail.com>
2025-09-26 11:54:18 +0200

    Adjust bundesland select component for dark mode

--- src/app/components/nebenkosten/bundeslandSelection.tsx
+++ src/app/components/nebenkosten/bundeslandSelection.tsx
@@ -9,7 +9,7 @@ type PropTypes = {
 export default function BundeslandSelection({bundesland, setBundesland, setShowMap}: PropTypes) {
   return (
     <div
-      className={`fixed z-40 h-fit w-full rounded-xl bg-radial-[at_50%_50%] from-[var(--background)] to-[var(--secondary)] md:static md:rounded-none md:rounded-l-xl`}
+      className={`fixed z-40 h-fit w-full rounded-xl bg-radial-[at_50%_50%] from-[var(--background)] to-[var(--secondary)] md:static md:rounded-none md:rounded-l-xl dark:to-[var(--background)]/20 dark:border-r`}
     >
       <div className="mx-10 mt-4 rounded-full bg-[var(--foreground)] p-2 text-sm text-[var(--background)] shadow-lg md:mb-4">
         <select
