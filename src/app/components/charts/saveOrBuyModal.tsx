8f58673487e434bd0b99ba20a699d61c22a53ab6
Jakob Lingel <jlingel68@gmail.com>
2025-09-26 13:05:18 +0200

    Style save or buy modal

--- src/app/components/charts/saveOrBuyModal.tsx
+++ src/app/components/charts/saveOrBuyModal.tsx
@@ -18,15 +18,15 @@ export default function SaveOrBuyModal({ table, input, setInput }: PropTypes) {
   };
 
   return (
-    <div className="rounded-lg bg-[var(--background)]/80 border p-10 shadow-xl">
-      <h4 className="mb-8 font-extrabold text-4xl">Kaufen oder Sparen?</h4>
-      <div className="flex flex-col gap-x-30 gap-y-10 lg:flex-row">
+    <div className="rounded-lg border bg-[var(--background)]/80 shadow-xl">
+      <h4 className="p-10 text-3xl border-b">Kaufen oder Sparen?</h4>
+      <div className="flex flex-col gap-x-30 gap-y-10 p-10 lg:flex-row">
         <OptionalParameters
           cashRoi={input.cash_roi!}
           handleInputChange={handleChange}
           rent={input.rent!}
         />
-        <div className="w-[400px] min-h-[280px]">
+        <div className="min-h-[280px] w-[400px]">
           <DevelopmentChart
             tilgungsTabelle={table}
             rent={input.rent}
@@ -34,6 +34,9 @@ export default function SaveOrBuyModal({ table, input, setInput }: PropTypes) {
             timeoutAmount={80}
           />
         </div>
+      </div>
+      <div className="px-10 py-4 text-[var(--foreground)]/70 rounded-b-xl text-sm border-t">
+        Inflation wird nicht berücksichtigt
       </div>
     </div>
   );
