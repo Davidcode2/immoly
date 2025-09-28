e31dca3b5a2561876ed676ad742d83fbd4ef85a6
Jakob Lingel <jlingel68@gmail.com>
2025-09-28 11:55:27 +0200

    Use shadcn select instead of default

--- src/app/components/nebenkosten/bundeslandSelection.tsx
+++ src/app/components/nebenkosten/bundeslandSelection.tsx
@@ -1,33 +1,40 @@
 import GermanyMap from "../germanyMap";
 import { bundeslaender } from "app/services/nebenkostenGrundsteuer";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 
 type PropTypes = {
   bundesland: string;
   setBundesland: (arg1: string) => void;
   setShowMap: (arg1: boolean) => void;
-}
-export default function BundeslandSelection({bundesland, setBundesland, setShowMap}: PropTypes) {
+};
+export default function BundeslandSelection({
+  bundesland,
+  setBundesland,
+  setShowMap,
+}: PropTypes) {
   return (
     <div
-      className={`fixed z-40 h-fit w-full rounded-xl bg-radial-[at_50%_50%] from-[var(--background)] to-[var(--secondary)] md:static md:rounded-none md:rounded-l-xl dark:to-[var(--primary)] md:dark:to-[var(--background)]/20 dark:border-r`}
+      className={`fixed z-40 h-fit w-full rounded-xl bg-radial-[at_50%_50%] from-[var(--background)] to-[var(--secondary)] md:static md:rounded-none md:rounded-l-xl dark:border-r dark:to-[var(--primary)] md:dark:to-[var(--background)]/20`}
     >
-      <div className="mx-10 mt-4 rounded-full bg-[var(--foreground)] dark:bg-[var(--background)] p-2 text-sm dark:text-[var(--foreground)] text-[var(--background)] shadow-lg md:mb-4">
-        <select
-          value={bundesland}
-          className="w-full px-2"
-          onChange={(e) => setBundesland(e.target.value)}
-        >
-          Wählen Sie Ihr Bundesland
-          {bundeslaender.map((_bundesland: string) => (
-            <option
-              key={_bundesland}
-              value={_bundesland}
-              defaultValue={bundesland}
-            >
-              {_bundesland}
-            </option>
-          ))}
-        </select>
+      <div className="mx-10 mt-4 text-sm text-[var(--foreground)] md:mb-4">
+        <Select value={bundesland} onValueChange={setBundesland}>
+          <SelectTrigger className="w-full rounded-full bg-[var(--background)] p-2 px-2 shadow-lg">
+            <SelectValue placeholder="Wählen Sie Ihr Bundesland" />
+          </SelectTrigger>
+          <SelectContent>
+            {bundeslaender.map((_bundesland: string) => (
+              <SelectItem key={_bundesland} value={_bundesland}>
+                {_bundesland}
+              </SelectItem>
+            ))}
+          </SelectContent>
+        </Select>
       </div>
       <div className="md:px-4 md:pb-4">
         <GermanyMap
