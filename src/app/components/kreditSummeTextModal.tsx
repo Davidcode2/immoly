e94d6e2705ad0c48ff2094e291b5ce13b4fc8a65
Jakob Lingel <jlingel68@gmail.com>
2025-10-06 19:32:53 +0200

    Adjust for mobile

--- src/app/components/kreditSummeTextModal.tsx
+++ src/app/components/kreditSummeTextModal.tsx
@@ -25,7 +25,7 @@ export default function KreditSummeModal({
   ).toLocaleString("de");
 
   return (
-    <div className="z-40 mx-4 rounded-xl border border-slate-500/20 bg-radial-[at_50%_75%] from-[var(--background)]/50 to-[var(--primary)]/20 p-6 pb-10 shadow-2xl backdrop-blur-3xl md:mx-auto md:max-w-3xl md:backdrop-blur-xl lg:p-20">
+    <div className="z-40 mx-4 rounded-xl border border-slate-500/20 bg-radial-[at_50%_75%] from-[var(--background)]/50 to-[var(--primary)]/20 p-10 pt-14 shadow-2xl backdrop-blur-3xl md:mx-auto md:max-w-3xl md:backdrop-blur-xl lg:p-20">
       <CloseButton onClick={onClose} />
       <div className="grid h-fit grid-cols-[1fr_2fr] items-baseline gap-x-2 gap-y-4">
         <span className="text-[var(--dark-accent)]/90 dark:text-[var(--ultralight-accent)]/90">
@@ -34,7 +34,7 @@ export default function KreditSummeModal({
         <span className="text-end text-2xl">
           {principal.toLocaleString("de")}&nbsp;€
         </span>
-        <span className="self-center text-[var(--dark-accent)]/90 dark:text-[var(--ultralight-accent)]/90">
+        <span className="text-[var(--dark-accent)]/90 dark:text-[var(--ultralight-accent)]/90">
           Nebenkosten
         </span>
         <div className="flex w-fit flex-col gap-x-4 justify-self-end border-b pb-4 text-end text-2xl text-[var(--dark-accent)]/90 dark:text-[var(--accent)]/90">
@@ -43,10 +43,10 @@ export default function KreditSummeModal({
             {percentageNebenkosten}&nbsp;%
           </div>
         </div>
-        <span className="text-xl font-normal self-center text-[var(--dark-accent)]/90 dark:text-[var(--ultralight-accent)]/90">
+        <span className="max-sm:text-lg text-xl font-normal self-center text-[var(--dark-accent)]/90 dark:text-[var(--ultralight-accent)]/90">
           Gesamtkosten
         </span>
-        <div className="text-end text-4xl">
+        <div className="text-end text-3xl lg:text-4xl">
           {sumTotal.toLocaleString("de")}&nbsp;€
         </div>
         <span className="group relative flex items-center gap-x-2 text-start text-[var(--dark-accent)]/90 dark:text-[var(--ultralight-accent)]/90">
@@ -76,8 +76,8 @@ export default function KreditSummeModal({
             {eigenkapitalPercentage}&nbsp;%
           </div>
         </span>
-        <h2 className="text-xl font-normal">Kreditsumme</h2>
-        <div className="text-end text-4xl">{kreditSummeString}&nbsp;€</div>
+        <h2 className="max-sm:text-lg text-xl font-normal">Kreditsumme</h2>
+        <div className="text-end text-3xl lg:text-4xl">{kreditSummeString}&nbsp;€</div>
       </div>
     </div>
   );
