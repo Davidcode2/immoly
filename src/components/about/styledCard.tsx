d797ab60a36e6b576d4cd5f17309ef9e01632dbc
Jakob Lingel <jlingel68@gmail.com>
2025-10-09 21:50:42 +0200

    Adjust colors

--- src/components/about/styledCard.tsx
+++ src/components/about/styledCard.tsx
@@ -1,3 +1,5 @@
+import { Check } from "lucide-react";
+
 export default function StyledCard({
   children,
   header,
@@ -7,12 +9,14 @@ export default function StyledCard({
 }) {
   return (
     <div className="h-full">
-      <div className="flex flex-col gap-x-6 rounded-lg bg-radial-[at_50%_75%] from-[var(--background)]/90 to-[var(--background)] backdrop-blur dark:shadow-[0_4px_50px_var(--success)]/20">
-        <div className="flex text-xl justify-start items-center gap-x-4 rounded-t-lg border-b px-10 py-8">
-          <div className="h-fit w-fit rounded-full bg-[var(--dark-accent)] p-6"></div>
+      <div className="flex flex-col gap-x-6 rounded-lg border bg-radial-[at_50%_75%] from-[var(--background)]/90 to-[var(--background)] shadow-xl backdrop-blur dark:shadow-[0_4px_50px_var(--foreground)]/20">
+        <div className="flex items-center justify-start gap-x-4 rounded-t-lg border-b px-10 py-8 text-xl">
+          <div className="h-fit w-fit rounded-full bg-[var(--dark-accent)] p-6">
+            <Check stroke="var(--background)" />
+          </div>
           {header}
         </div>
-        <div className="px-6 py-3 xl:pr-40 xl:pl-10 xl:py-10">{children}</div>
+        <div className="px-6 py-3 xl:py-10 xl:pr-48 xl:pl-10">{children}</div>
       </div>
     </div>
   );
