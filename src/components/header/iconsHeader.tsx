d07f40e0c1be6958dd364d9aa049ff7a652b1195
Jakob Lingel <jlingel68@gmail.com>
2025-10-11 16:33:26 +0200

    Add site menu

--- src/components/header/iconsHeader.tsx
+++ src/components/header/iconsHeader.tsx
@@ -1,10 +1,12 @@
 import DarkModeToggle from "../darkModeToggle";
+import SiteMenu from "./siteMenu";
 
 export default function IconsHeader() {
 
   return (
     <div className="flex gap-2">
       <DarkModeToggle/>
+      <SiteMenu/>
     </div>
   );
 }
