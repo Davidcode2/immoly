8cc0d6a3d8d6eee651c71aba5503a2e996fa28a1
Jakob Lingel <jlingel68@gmail.com>
2025-10-12 17:27:21 +0200

    Remove blur on hero

--- src/components/hero/hero.tsx
+++ src/components/hero/hero.tsx
@@ -3,7 +3,7 @@ import CursorSwarm from "./cursor-swarm";
 export default function Hero() {
   return (
     <div className="rounded-lg border-[var(--accent)]">
-      <div className="rounded-lg p-4 backdrop-blur md:p-8">
+      <div className="rounded-lg p-4 md:p-8">
         <div className="absolute left-48">
           <CursorSwarm />
         </div>
