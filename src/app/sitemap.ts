28fe27758bb14c47934364626ddbe552713b340f
Jakob Lingel <jlingel68@gmail.com>
2025-10-12 14:23:12 +0200

    Add impressum to sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -14,5 +14,11 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: 'monthly',
       priority: 0.8,
     },
+    {
+      url: 'https://immoly.io/impressum',
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 0.4,
+    },
   ]
 }
