c86db3074ca358cf3789f95cefeec706f5d5ad04
Jakob Lingel <jlingel68@gmail.com>
2025-10-12 11:40:15 +0200

    Navigation in footer

--- src/components/header/AnimatedMenuEntry.tsx
+++ src/components/header/AnimatedMenuEntry.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { motion } from "framer-motion";
-import { ArrowRight } from "lucide-react";
+import { ArrowLeft, ArrowRight } from "lucide-react";
 import { usePathname } from "next/navigation";
 import Link from "next/link";
 
@@ -8,10 +8,12 @@ export default function AnimatedMenuEntry({
   href,
   text,
   onSelect,
+  arrowPosition = "left",
 }: {
   text: string;
   href: string;
   onSelect?: () => void;
+  arrowPosition?: "left" | "right";
 }) {
   const [hovered, setHovered] = useState(false);
   const currentPath = usePathname();
@@ -25,20 +27,28 @@ export default function AnimatedMenuEntry({
       onMouseLeave={() => setHovered(false)}
     >
       {selected ? (
-        <ArrowRight className="absolute left-0 mr-6" />
+        <span
+          className={`${arrowPosition === "left" ? "absolute left-0 mr-6" : "order-2 ml-2"}`}
+        >
+          {arrowPosition === "left" ? <ArrowRight /> : <ArrowLeft />}
+        </span>
       ) : (
         <>
           <motion.span
             initial={{ opacity: 0, x: -16 }}
             animate={hovered ? { opacity: 1, x: 0 } : { opacity: 0, x: -16 }}
             transition={{ type: "spring", stiffness: 400, damping: 30 }}
-            className="absolute left-0 mr-6"
+            className={`${arrowPosition === "left" ? "absolute left-0 mr-6" : "order-2 ml-2"}`}
           >
-            <ArrowRight stroke="var(--accent)" />
+            {arrowPosition === "left" ? (
+              <ArrowRight stroke="var(--accent)" />
+            ) : (
+              <ArrowLeft stroke="var(--accent)" />
+            )}
           </motion.span>
         </>
       )}
-      <li className="inline-block pl-8">
+      <li className={` ${arrowPosition === "left" && "pl-8" } inline-block `}>
         <Link href={href} onClick={() => onSelect !== undefined && onSelect()}>
           {text}
         </Link>
