66189ff277859e27f764ae601f10d86fa9ea92fe
Jakob Lingel <jlingel68@gmail.com>
2025-10-23 11:39:33 +0200

    Add debounce to resizeObserver for embed

--- src/components/embed.tsx
+++ src/components/embed.tsx
@@ -1,17 +1,22 @@
 "use client";
 
 import ResultDisplay from "@/resultDisplay";
+import { debounce } from "@/utils/debounce";
 import { useEffect } from "react";
 
 export default function Embed() {
   useEffect(() => {
+    debounce(createResizeObserver, 300)();
+  }, []);
+
+  const createResizeObserver = () => {
     new ResizeObserver(() => {
       window.parent.postMessage(
         { type: "IMMOLY_IFRAME_HEIGHT", height: document.body.scrollHeight },
         "*",
       );
     }).observe(document.body);
-  }, []);
+  };
 
   return (
     <div className="p-3 md:pt-8">
