c2216c7274cad96f89c213577bba50dc3e99c680
Jakob Lingel <jlingel68@gmail.com>
2025-11-26 14:17:53 +0100

    Fix icon spillover

--- src/components/prices/bulletPointItem.tsx
+++ src/components/prices/bulletPointItem.tsx
@@ -8,9 +8,9 @@ export default function BulletPointItem({ plus, children }: PropTypes) {
   return (
     <div className="flex gap-x-2 lg:text-sm xl:text-base">
     { plus ?
-      <span className="inline-block rounded-full dark:bg-green-900/20 bg-green-50"><PlusIcon /></span>
+      <span className="inline-block h-fit rounded-full dark:bg-green-900/20 bg-green-50"><PlusIcon /></span>
     : 
-      <span className="inline-block rounded-full dark:bg-purple-950/40 bg-purple-50"><Check /></span>
+      <span className="inline-block rounded-full h-fit dark:bg-purple-950/40 bg-purple-50"><Check /></span>
     }
     { children }
     </div>
